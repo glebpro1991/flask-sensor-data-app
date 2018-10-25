05b9f76b43f57066d447ac560667ce6aaa836d15
Glebs <jjb15133@uni.strath.ac.uk>
2018-10-25 01:30:29 +0100

    database changes

--- app.py
+++ app.py
@@ -4,25 +4,17 @@ from models import db, SensorDataModel
 app = Flask(__name__)
 
 
-# POSTGRES = {
-#     'user': 'postgres',
-#     'pw': 'password',
-#     'db': 'sensors',
-#     'host': 'localhost',
-#     'port': '5432',
-# }
-#
-#
-# # POSTGRES = {
-# #     'user': 'wopxbjuaxlgutd',
-# #     'pw': 'ed84051e3ee744aed0024048731bcfc17b709b6f4d3c44609beb71587b19b75b',
-# #     'db': 'd9bttgahlprgqp',
-# #     'host': 'ec2-54-83-49-109.compute-1.amazonaws.com',
-# #     'port': '5432',
-# # }
-#
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-# %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+POSTGRES = {
+    'user': 'srqqtgrpbtauoh',
+    'pw': 'ed84051e3ee744aed0024048731bcfc17b709b6f4d3c44609beb71587b19b75b',
+    'db': 'd7etse1b52ls3r',
+    'host': 'ec2-54-243-187-30.compute-1.amazonaws.com',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 
 db.init_app(app)
