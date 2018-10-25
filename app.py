70e166b642f6ea3ac7a44f962e187ccf0c2ffecb
Glebs <jjb15133@uni.strath.ac.uk>
2018-10-25 01:04:42 +0100

    database configs

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from models import db, SensorDataModel
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'sensors',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+
+# POSTGRES = {
+#     'user': 'wopxbjuaxlgutd',
+#     'pw': 'ed84051e3ee744aed0024048731bcfc17b709b6f4d3c44609beb71587b19b75b',
+#     'db': 'd9bttgahlprgqp',
+#     'host': 'ec2-54-83-49-109.compute-1.amazonaws.com',
+#     'port': '5432',
+# }
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+
+db.init_app(app)
+
+
+@app.route('/', methods=['GET'])
+def present():
+    return "Hello darkness my old friend!"
+
+
+# first endpoint
+@app.route('/save', methods=['POST'])
+def save():
+    req_data = request.get_json()
+    sensor_data = SensorDataModel(req_data)
+    sensor_data.save()
+    return 'OK'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+db = SQLAlchemy()
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        return {
+            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
+
+class SensorDataModel(BaseModel):
+    __tablename__ = 'data'
+    id = db.Column(db.Integer, primary_key=True)
+    accX = db.Column(db.Float)
+    accY = db.Column(db.Float)
+    accZ = db.Column(db.Float)
+    gyroX = db.Column(db.Float)
+    gyroY = db.Column(db.Float)
+    gyroZ = db.Column(db.Float)
+    magX = db.Column(db.Float)
+    magY = db.Column(db.Float)
+    magZ = db.Column(db.Float)
+
+    def __init__(self, data, *args):
+        super().__init__(*args)
+        self.accX = data.get('accX')
+        self.accY = data.get('accY')
+        self.accZ = data.get('accZ')
+        self.gyroX = data.get('gyroX')
+        self.gyroY = data.get('gyroY')
+        self.gyroZ = data.get('gyroZ')
+        self.magX = data.get('magX')
+        self.magY = data.get('magY')
+        self.magZ = data.get('magZ')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
